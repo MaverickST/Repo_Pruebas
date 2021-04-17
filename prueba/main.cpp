51e9066c59d6bb7cb844fe7f6e035f1f3e7dfd02
MaverickST <maverick.sossa@udea.edu.co>
2021-04-16 22:12:55 -0500

    Agreso una restica

--- prueba/main.cpp
+++ prueba/main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main()
 {
    //DOCUMENTO DE PRUEBAS
+    int b = 3, a = 2;
+    cout << "La suma es: " << b - a << endl;
     cout << "Hello World!" << endl;
     return 0;
 }
