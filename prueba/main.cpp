c4feba53782b1f7622f55d1fd2cce802a78b2161
MiguelGil1 <miguel.gil1@udea.edu.co>
2021-04-16 22:25:54 -0500

    Merge

--- prueba/main.cpp
+++ prueba/main.cpp
@@ -5,8 +5,16 @@ using namespace std;
 int main()
 {
    //DOCUMENTO DE PRUEBAS
-    int b = 3, a = 2;
-    cout << "La suma es: " << b - a << endl;
+    int x = 3, y = 2;
+    cout << "La suma es: " << x - y << endl;
     cout << "Hello World!" << endl;
+    int a, b;
+    cout << "Ingrese un numero: ";
+    cin >> a;
+    cout << "Ingrese otro numero: ";
+    cin >> b;
+    cout << "La suma de " << a << " + " << b << " es: " << a+b;
     return 0;
+    //blablablablablabla
+    //Esto es un comentario
 }
